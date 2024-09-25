6bbe69055a26a7bb36c506937fc27da20a0771da
Andrew Mott <andrew@andrewmott.com>
2024-09-25 01:25:11 -0400

    Adds in memory postgres database
    - Moves database creation to an external file
    - Still needs testing to confirm this is working

--- __tests__/server/userController.test.js
+++ __tests__/server/userController.test.js
@@ -1,22 +1,30 @@
+/**
+ * Some versions of node don't have a TextEncoder / TextDecoder set as
+ * enviroment variables.  This problem arose once I changed jest.mock to 
+ * jest.doMock to prevent it hoisting to the top.  I'm not sure if that also 
+ * somehow altered these enviroment variables.  This is a sort of "polyfil"
+ * to provide those values globally.
+ * 
+ * see https://www.dhiwise.com/post/how-to-resolve-the-error-textencoder-is-not-defined-jes
+ */
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 const request = require('supertest');
-const { newDb } = require('pg-mem');
 
 const app = require('../../server/server');
+const mockDatabase = require('./mockDatabase');
 
 
-/*
- * This testing method spins up a in memory postgres database for our endpoints
- * to test against.  We could have mocked the expected database responses 
- * completely in Jest, but here we are attempting to provide a more robust test
- * that is perhaps easier to maintain, as each database query will not need to
- * be mocked, and we can more easily update any schema changes.  The cost to 
- * this approach is test run times, but since our project is small, this might 
- * not be a problem.
+
+/* 
+ * Here are are mocking up the entire pg module so that we can have it use the
+ * in memory postgres database, and the rest of our code will use it for
+ * querying the database
  */
 
-const db = newDb();
-
-// create a schema for the database.  get this from the actual database
+jest.doMock('pg', () => mockDatabase());
 
 // before all tests create the database
 // after all tests maybe get rid of the database
@@ -24,12 +32,9 @@ const db = newDb();
 // beforeEach maybe insert some fake data into the database for testing
 
 
-jest.mock('pg', () => {
-  return {
-    Pool: jest.fn(() => {
-      return { query: (text, params) => db.public.manyOrNone(text, params) };
-    })
-  }
+
+
+// dummy test just while setting up pg-mem
+describe('userController tests', () => {
+  test('this is a test', () => expect(1).toBe(1));
 });
-
-
