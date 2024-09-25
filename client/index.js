3bc42a1fa8be858a30df6bdc8cae2c488f1de504
Timothy <teecheng5@gmail.com>
2024-09-25 11:28:09 -0700

    created redux store, slice for user

--- client/index.js
+++ client/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
