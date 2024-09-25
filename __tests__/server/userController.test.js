bbc921001fa861570e79e7f786dddfad984cf93d
Andrew Mott <andrew@andrewmott.com>
2024-09-25 16:23:05 -0400

    Adds both table creation

--- __tests__/server/userController.test.js
+++ __tests__/server/userController.test.js
@@ -2,27 +2,35 @@ const request = require('supertest');
 const db = require('../../server/models/usersModel');
 
 /*
- * Lets make sure we are in test mode in case this is test gets executed  of 
- * npm run test:server
+ * Lets make sure we are in "test mode", so that we don't accidently operate
+ * on the real database
  */
 if (process.env.NODE_ENV != 'server-test') {
   throw new Error("Tests must be run in the testing environment");
 }
 
-
 beforeAll(async () => {
   try {
     db.query(`
       CREATE TABLE IF NOT EXISTS users (
-        user_id uuid DEFAULT gen_random_uuid() NOT NULL,
+        user_id uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
         firstname text NOT NULL,
-        email text NOT NULL,
+        email text NOT NULL UNIQUE,
         password text NOT NULL,
         city text,
         zipcode integer,
         gender text,
         phone bigint
-    );`
+      );
+
+      CREATE TABLE IF NOT EXISTS useractivities (
+        "useractivityId" uuid DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY,
+        user_id uuid DEFAULT gen_random_uuid(),
+        activityname character varying,
+        skilllevel character varying,
+        FOREIGN KEY (user_id) 
+        REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+      );`
     );
   }
   catch (error) {
