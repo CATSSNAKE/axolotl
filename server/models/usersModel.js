0a98956f7de90f7490f55cd023adbd8feea94005
Andrew Mott <andrew@andrewmott.com>
2024-09-25 10:07:57 -0400

    Changes from in memory to local postgres server for testing
    - pg-mem had errors that seemed to be issues with module itself
    - sets up new test in test:server with environment vars and config files

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,9 +1,19 @@
 const { Pool } = require('pg'); // Use require('pg') if using CommonJS
 
-const PG_URI =
-  process.env.DATABASE_URI ||
-  'postgresql://postgres.qjdawteymmlljerttghu:outdoor12buddies34@aws-0-us-west-1.pooler.supabase.com:6543/postgres'; // Update with your connection string
+let PG_URI;
+if (process.env.NODE_ENV === 'server-test') {
+  if (!process.env.DATABASE_TEST_URI) {
+    throw Error("No test database connection string found");
+  }
+  PG_URI = process.env.DATABASE_TEST_URI;
+}
+else {
+  PG_URI =
+    process.env.DATABASE_URI ||
+    'postgresql://postgres.qjdawteymmlljerttghu:outdoor12buddies34@aws-0-us-west-1.pooler.supabase.com:6543/postgres'; // Update with your connection string
+}
 
+PG_URI = 'postgresql://test-user:JX2xDwKZRorKbvJaXwL6@127.0.0.1:5432/axolotl-test';
 const pool = new Pool({
   connectionString: PG_URI,
 });
